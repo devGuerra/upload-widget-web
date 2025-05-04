59413d517d33d777bc657da9a7c1acc54ffaeb91
Roberto Guerra <contato@robertoguerra.dev>
2025-05-04 13:20:33 +0100

    chore: Botão e widget colapsável

---
+++ src/components/upload-widget-header.tsx
@@ -0,0 +1,16 @@
+import { Minimize2 } from "lucide-react";
+import * as Collapsible from "@radix-ui/react-collapsible";
+import { Button } from "./ui/button";
+
+export function UploadWidgetHeader() {
+	return (
+		<div className="w-full p-4 py-2 bg-white/2 border-zinc-800 border-b flex items-center justify-between ">
+			<span className="text-small font-medium">Upload files</span>
+			<Collapsible.Trigger asChild>
+				<Button size="icon" className="-mr-2">
+					<Minimize2 className="size-4" strokeWidth={1.5} />
+				</Button>
+			</Collapsible.Trigger>
+		</div>
+	);
+}
